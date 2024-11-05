0584a7296d0793b8f53f9055267e2929751308ca
Yunhui Song <202402597@post.au.dk>
2024-11-05 21:54:08 +0100

    add test

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -14,18 +14,18 @@ export default function TabOneScreen() {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold'
+    },
+    separator: {
+        marginVertical: 30,
+        height: 1,
+        width: '80%'
+    }
 });
